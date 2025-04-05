332f652227129257cfe7baec40b02668d34bf16b
patson120 <patrick1kenne@gmail.com>
2025-04-05 08:22:54 +0100

    Mise à jour

--- src/components/sections/HeaderSection/HeaderSection.tsx
+++ src/components/sections/HeaderSection/HeaderSection.tsx
@@ -1,38 +1,40 @@
+
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { BellIcon } from "lucide-react";
-import React, { JSX } from "react";
+import Link from "next/link";
+import { JSX } from "react";
 
- const HeaderSection = (): JSX.Element => {
+export default function HeaderSection (): JSX.Element  {
   return (
     <header className="w-full h-16 bg-white flex items-center justify-between px-9">
       <div className="flex items-center gap-2">
-        <Avatar className="w-10 h-10 rounded-full overflow-hidden">
+        <Avatar className="w-10 h-10 rounded-full flex justify-center items-center overflow-hidden">
           <AvatarImage src="/assets/img/image.png" alt="Profile" />
-          <AvatarFallback>HG</AvatarFallback>
+          <AvatarFallback className="text-lg font-bold border border-gray-600 rounded-full">HG</AvatarFallback>
         </Avatar>
 
         <div className="flex flex-col justify-center items-start gap-0.5">
-          <div className="font-body-3 text-start font-normal text-noir-dashboard text-sm leading-[14px]">
+          <h3 className="font-body-3 text-start font-normal text-noir-dashboard text-sm leading-[14px]">
             Hadrien Gayap
-          </div>
-          <div className="font-legend text-gray text-xs">
+          </h3>
+          <p className="font-legend text-gray text-xs">
             Super administrateur
-          </div>
+          </p>
         </div>
       </div>
 
       <div className="flex items-center gap-6">
-        <Button
-          variant="outline"
-          className="h-10 text-noir-dashboard font-body-3 font-normal"
-        >
-          Voir le site internet
-        </Button>
+        <Link href={"https://diocese-de-moncton.vercel.app/"} target="_blank" >
+          <Button
+            variant="outline"
+            className="h-10 text-noir-dashboard font-body-3 font-normal"
+          >
+            Voir le site internet
+          </Button></Link>
         <BellIcon className="w-6 h-6 text-noir-dashboard" />
       </div>
     </header>
   );
 };
 
-export default HeaderSection;
