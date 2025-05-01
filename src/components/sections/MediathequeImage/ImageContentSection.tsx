d2ec5c472726469937d0f63bed30177505f48e47
patson120 <patrickkennenl@gmail.com>
2025-05-01 06:35:54 +0100

    Intégration de l'api de suppression des images et ajustement de l'interface graphique

--- src/components/sections/MediathequeImage/ImageContentSection.tsx
+++ src/components/sections/MediathequeImage/ImageContentSection.tsx
@@ -8,21 +8,39 @@ import { Image as ImageType } from '../../../../types';
 import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { useEffect, useState } from 'react';
-import { InfoIcon } from 'lucide-react';
+import { Eye, InfoIcon, Trash2Icon } from 'lucide-react';
+import { Loader } from '@/components/ui/loader';
 
 export default function ImageContentSection() {
     
    const [images, setImages] = useState<ImageType[]>([])
    const [selectedImage, setSelectedImage] = useState<ImageType | undefined>()
+   const [isDeleting, setIsDeleting] = useState(false)
 
     useEffect(() => {
      ( async () => {
         const response: ImageType[] = await apiClient.get('/api/galeries');
-        setImages(response.filter(item => !item.path.toString().includes("https")))
+        setImages(response)
      }) ()
     }, [])
-    
 
+    const handleDeleteImage = async (img?: ImageType) => {
+      if (isDeleting) return;
+      if (img) {
+        setIsDeleting(true);
+        try {
+          await apiClient.delete(`/api/galeries/${ img.id}`);
+          setImages(images.filter(image => image.id !== img.id));
+          setSelectedImage(undefined);
+        } catch (error) {
+          console.error('Error deleting image:', error);
+        }
+        finally {
+          setIsDeleting(false);
+        }
+      }
+    };
+    
     return (
         <section className="w-full flex-1 p-6">
             <div className="flex flex-col bg-white w-full items-start gap-6 rounded-2xl p-6">
@@ -32,8 +50,7 @@ export default function ImageContentSection() {
                         {images.map((image, index) => (
                             <Card
                                 key={index}
-                                onClick={() => {setSelectedImage(image)}}
-                                className="overflow-hidden rounded-lg border-none relative cursor-pointer shrink-0 min-h-[150px] max-h-[200px]">
+                                className="overflow-hidden rounded-lg border-none relative shrink-0 min-h-[150px] max-h-[200px]">
                                 <Image
                                     alt={`Image ${index + 1}`}
                                     src={`${process.env.NEXT_PUBLIC_API_URL}/${image.path!}`}
@@ -41,6 +58,16 @@ export default function ImageContentSection() {
                                     fill
                                     priority
                                 />
+                                <div className='absolute top-0 left-0 w-full h-full bg-black/30 flex items-center justify-center opacity-0 hover:opacity-100 transition-all duration-300 ease-in-out'>
+                                    <div className='flex items-center gap-2'>
+                                      <button onClick={() => {setSelectedImage(image)}} className='h-10 w-10 rounded-full bg-white/20 text-white hover:bg-white hover:text-black flex items-center justify-center'>
+                                        <Eye className='w-5 h-5'/>
+                                      </button>
+                                      <button onClick={() => handleDeleteImage(image)} className='h-10 w-10 rounded-full bg-white/20 text-white hover:bg-white hover:text-black flex items-center justify-center'>
+                                        { isDeleting ? <Loader className="h-5 w-5" /> : <Trash2Icon className='w-5 h-5'/>}
+                                      </button>
+                                    </div>
+                                </div>
                             </Card>
                         ))}
                     </div>
@@ -67,13 +94,14 @@ export default function ImageContentSection() {
                     <div className='flex gap-3'>
                       <Button onClick={() => setSelectedImage(undefined)} className="px-3.5 py-0 bg-blue text-white rounded-[7px]">
                         <span className="font-body-3 whitespace-nowrap">
-                            Fermer
+                          Fermer
                         </span>
                       </Button>
                       <Button
-                        onClick={() => {}}
+                        onClick={() => handleDeleteImage(selectedImage)}
                         variant="outline"
                         className=" p-3.5 bg-white rounded-lg border border-solid border-[#d9d9d9]">
+                        { isDeleting && <Loader className="h-5 w-5 mr-2" />}
                         <span className="font-body-3 text-noir-dashboard whitespace-nowrap">
                             Supprimer
                         </span>
