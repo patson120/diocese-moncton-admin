aca7c30588a0cd88260a0f0ce7d271020725ba8d
patson120 <patrickkennenl@gmail.com>
2025-05-26 16:22:57 +0100

    Mise à jour de la page du login

--- src/app/(auth)/login/ConnexionPage.tsx
+++ src/app/(auth)/login/ConnexionPage.tsx
@@ -66,7 +66,7 @@ export const ConnexionPage = (): JSX.Element => {
       <div className="grid grid-cols-5 gap-4 md:gap-10 md:m-3 w-full">
         <div className="col-span-full md:col-span-2 relative flex flex-col justify-center items-center">
           {/* Login form container */}
-          <div className="flex flex-col w-2/3 gap-5 lg:gap-10">
+          <div className="flex flex-col md:w-2/3 gap-5 lg:gap-10">
             {/* Logo */}
             <div className="w-full flex justify-center items-center">
               <div className="relative w-[160px] h-[95px] shrink-0">
@@ -82,7 +82,7 @@ export const ConnexionPage = (): JSX.Element => {
 
             {/* Notice box */}
             <Card className="w-full bg-[#f2f2f9] rounded-xl">
-              <CardContent className="px-[15px] py-2.5">
+              <CardContent className="px-2 md:px-[15px] py-2.5">
                 <p className="font-body-3 text-blue text-center">
                   Cette espace est reservé uniquement aux membres du
                   diocèse de Moncton
