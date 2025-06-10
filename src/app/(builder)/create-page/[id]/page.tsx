f6d77f0488b0613285c0a1487e1d78c99834ebd5
patson120 <patrickkennenl@gmail.com>
2025-06-10 04:42:35 +0100

    Update

--- src/app/(builder)/create-page/[id]/page.tsx
+++ src/app/(builder)/create-page/[id]/page.tsx
@@ -1,18 +1,14 @@
-// import { PageBuilder } from '@/components/pages/page-builder/page-builder';
+import { PageBuilder } from '@/components/pages/page-builder/page-builder';
 
-// export default function BuilderPage({ params }: { params: { id: string } }) {
-//   const id = params?.id! === 'new' ? 'new' : params.id;
-//   
-//   return <PageBuilder pageId={id} />;
-// }
+export default function BuilderPage({ params }: { params: { id: string } }) {
+  const id = params?.id! === 'new' ? 'new' : params.id;
+  
+  return <PageBuilder pageId={id} />;
+}
 
 // This function is required for static site generation
-// export async function generateStaticParams() {
-//   return [{ id: 'new' }];
-// }
-
-export default function page() {
-  return (
-    <div>page</div>
-  )
+export async function generateStaticParams() {
+  return [{ id: 'new' }];
 }
+
+
