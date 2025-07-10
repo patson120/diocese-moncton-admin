55b10866c521bdab93c52991517fc45cb6cbe363
patson120 <patrickkennenl@gmail.com>
2025-07-10 10:26:19 +0100

    Ajout de l'image de couverture au message de l'archevêque

--- src/components/shared/MessageComp.tsx
+++ src/components/shared/MessageComp.tsx
@@ -2,11 +2,24 @@
 import { formatDateToLocal } from "@/lib/utils"
 import { Message } from "../../app/types"
 import Text from "./Text"
+import Image from "next/image"
 
 const MessageComp = ({ message, onClick }: { message: Message, onClick?: (e: any) => void }) => {
     return (
-        <div onClick={onClick} className='bg-[#F9F9F0] cursor-pointer rounded-xl flex flex-col justify-between gap-[10px] px-5 py-6'>
+        <div onClick={onClick} className='bg-[#F9F9F0] cursor-pointer rounded-xl overflow-hidden flex flex-col justify-between gap-[10px] px-5 py-6'>
             <div className="">
+                {
+                    message.image === null ? ( 
+                    <div
+                        className="w-full h-[150px] rounded-xl bg-cover bg-center"
+                        style={{ backgroundImage: `url("/vector.svg")` }}
+                    />
+                    ) : (
+                    <div
+                        className="w-full h-[150px] rounded-xl bg-cover bg-center"
+                        style={{ backgroundImage: `url(${process.env.NEXT_PUBLIC_API_URL}/${message.image})` }}
+                    />
+                )}
                 <div className='body-1 font-bold text-black line-clamp-2'>
                     <Text labelFr={message.titre_fr} labelEn={message.titre_en} />
                 </div>
