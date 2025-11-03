a283d7e2a2940d643bb9095e6299c310cd754507
patson120 <patrickkennenl@gmail.com>
2025-11-03 10:57:30 +0100

    Update

--- src/components/sections/ClergPretres/EditMemberFormSection.tsx
+++ src/components/sections/ClergPretres/EditMemberFormSection.tsx
@@ -152,14 +152,14 @@ const EditMemberFormSection = ({memberData} : { memberData: Member}): JSX.Elemen
 
     try {
 
-      const recaptchaReponse = await verifyRecaptchaToken();
+      /* const recaptchaReponse = await verifyRecaptchaToken();
       const recaptchaData = await recaptchaReponse.json();
 
       if (!recaptchaData.success) {
         toast.error(recaptchaData.message || 'Erreur de vérification reCAPTCHA');
         setIsloading(false);
         return;
-      }
+      } */
 
       const response: any = await apiClient.post(`/api/membres/${memberData.id}?_method=PUT`, formdata, {
         'Content-Type': 'multipart/form-data'
@@ -423,15 +423,15 @@ const EditMemberFormSection = ({memberData} : { memberData: Member}): JSX.Elemen
                     </FormItem>
                   )}
                 />
-                <ReCAPTCHA
+                {/* <ReCAPTCHA
                   sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ''}
                   onChange={handleRecaptchaChange}
-                />
+                /> */}
                 <div className="flex flex-row gap-4">
                   <Button variant={'outline'} onClick={() => setStep(1)} className="w-1/3 mt-8 h-12 rounded-lg">
                     Retour
                   </Button>
-                  <Button disabled={ isLoading || !captchaToken } type="submit" className="w-2/3 h-12 mt-8 bg-blue text-white rounded-lg">
+                  <Button disabled={ isLoading } type="submit" className="w-2/3 h-12 mt-8 bg-blue text-white rounded-lg">
                     { isLoading && <Loader className='text-white mr-2' /> }
                     Mettre à jour
                   </Button>
