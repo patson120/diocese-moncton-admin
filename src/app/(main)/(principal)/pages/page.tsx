86aa0881f3fcf943157175c05b3781d47b4c3852
patson120 <patrickkennenl@gmail.com>
2026-02-11 02:25:35 -0400

    Update pages

--- src/app/(main)/(principal)/pages/page.tsx
+++ src/app/(main)/(principal)/pages/page.tsx
@@ -1,7 +1,10 @@
 import { ContentSection } from '@/components/sections/ContentSection/ContentSection'
+import { Suspense } from 'react'
 
 export default function page() {
   return (
-    <ContentSection />
+    <Suspense fallback={ <div className='h-[90vh] w-full flex justify-center items-center'><p className='text-center text-muted-foreground'>Loading</p> </div> }>
+      <ContentSection />
+    </Suspense>
   )
 }
